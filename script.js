ad256276b4b563faa8d6a1546cca718fc8ed8037
ahamathbasha <bashachristopherallah786@gmail.com>
2024-11-27 06:53:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// 1. precedency , how js runs our code,Read evaluate print loop.
+/*
+console.log("hello world")
+console.log(5+6)
+
+console.log(5+6 - 4/2)
+console.log(5+5-4*5)
+console.log(5+6-4/2*4)
+console.log(8-(5+6))
+*/
+
+//2.primitive datatype
+
+/*
+let no=123
+console.log(typeof no)
+let pointno=12.34
+console.log(typeof pointno)
+let negativeno=-20
+console.log(typeof negativeno)
+let str="hello"
+console.log(typeof str)
+let emptystr=''
+console.log(typeof emptystr)
+let backtick=``
+console.log(typeof backtick)
+//backtick will print the output based on how we write it.
+console.log(`hello my name is basha 
+    ahamathbasha`)
+console.log(typeof '100')
+let checkNoOrString=+'100'
+console.log(typeof checkNoOrString)
+console.log(checkNoOrString)
+let checkNoOrString2=100+''
+console.log(typeof checkNoOrString2)
+console.log(checkNoOrString2)
+let checkNoOrString3=''+100
+console.log(typeof checkNoOrString3)
+console.log(checkNoOrString3)
+console.log(typeof 100+'')
+console.log(typeof (100+''))
+console.log(+true)
+console.log(+false)
+console.log(typeof undefined)
+console.log(+undefined)
+console.log(typeof null)
+console.log(+null)
+let bigint=BigInt(123456789000000999999999)
+console.log(bigint)
+console.log(typeof bigint)
+//console.log(+bigint) we cannot convert bigint to no because both are used for different purpose
+console.log(''+bigint)
+let symbol1=Symbol("description")
+let symbol2=Symbol("description")
+console.log(symbol1 === symbol2)
+console.log(typeof symbol1)
+//console.log(+symbol1)we cannot convert symbol into no
+//console.log(''+symbol1)we cannot convert symbol into string also
+
+*/
