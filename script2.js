dd3847a32652e0fbea0badfe1f72ce79af331257
ahamathbasha <bashachristopherallah786@gmail.com>
2024-12-05 09:23:31 +0530

    while loop and combined assignment operator

--- script2.js
+++ script2.js
@@ -195,4 +195,120 @@ shallowCopy[1].address.city = "Hyderabad";
 console.log(people[1].address.city);
 
 //output:"hyderabad"
+
+
+//22.combined assignment operator or compound operators
+
+//+=,-=.*=
+
+let num=5
+//num=num+2//or
+num+=2
+console.log(num)
+num+=2
+console.log(num)
+
+//when you put debugger and hover on the vairable it will increase the value it is a bug.Just know about it.
+
+let num2=2
+num2*=2
+console.log(num2)
+
+let num3=10
+num3-=2
+console.log(num3)
+
+let num4=10
+num4/=2
+console.log(num4)
+
+let num5=10
+num5%=2
+console.log(num5)
+
+let num6=2
+num6**=2
+console.log(num6)
+
+//increment
+let n=1
+n++//increase one digit at a time
+console.log(n)//
+
+//check this line of code in chrome browser
+//let no=5
+//post increment
+//no++//it will give result or current no after it increase the value and store in that place
+//no++ 5
+//no 6
+
+//pre increment
+// let no=5
+// ++no//6
+// no //6
+//++n//it will increase the value and store in the place 
+
+let number=5
+
+const newNumber=number++//5++ 5 will be stored in newNumber.After that it increases the number variable
+console.log(newNumber)
+console.log(number)
+
+let number2=5
+const newNumber2=++number2//++5 6 will stored in that newNumber2 and number2 will be increased
+console.log(newNumber2)
+console.log(number2)
+
+let postMinus1=5
+
+const postMinus2=postMinus1--
+console.log(postMinus2)//5
+console.log(postMinus1)//4
+
+let postMinus3=5
+const postMinus4=--postMinus3
+console.log(postMinus4)//4
+console.log(postMinus3)//4
+
+//Here when you decrease or increase the variable address will be changed always performing increment or decrement operation.
+
+//23.while loop
+
+//true or false
+//in while conditon if we set true it won't stop until it got false
+let num=1
+while(num <= 10){
+    console.log(num)
+    num++
+}
+
+let no_One=1
+while(no_One<100){
+    console.log(no_One)
+    no_One++ //(or) no_One=no_One+1
+}
+    
+const friends=['Akash','Rahul','Adarsh','Arif','Gaurav']
+let i=0
+// let length=friends.length-1
+// while(i<length){
+//     console.log(friends[i])
+//     i++
+// }
+
+while(i<friends.length){
+    console.log(`${i+1}.${friends}`)
+    i++
+}
+
+
+const friends=['Akash','Rahul','Adarsh','Arif','Gaurav']
+
+let i=0
+
+while(i<friends.length){
+    friends[i]=(friends[i]+" "+"kumar")
+    i++
+}
+console.log(friends)
 */
